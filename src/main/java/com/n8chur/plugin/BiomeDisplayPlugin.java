df37a1d954ec4750e50620141d6d971be4fa6abe
Westin Newell <westin.newell@gmail.com>
2026-01-15 14:24:12 -0800

    Remove ExampleCommand and update plugin setup
    Deleted ExampleCommand.java and removed its registration from BiomeDisplayPlugin. Updated log messages for plugin initialization and setup to improve clarity.

--- src/main/java/com/n8chur/plugin/BiomeDisplayPlugin.java
+++ src/main/java/com/n8chur/plugin/BiomeDisplayPlugin.java
@@ -6,22 +6,17 @@ import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
 import javax.annotation.Nonnull;
 
-/**
- * This class serves as the entrypoint for your plugin. Use the setup method to register into game registries or add
- * event listeners.
- */
 public class BiomeDisplayPlugin extends JavaPlugin {
 
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
     public BiomeDisplayPlugin(@Nonnull JavaPluginInit init) {
         super(init);
-        LOGGER.atInfo().log("Hello from " + this.getName() + " version " + this.getManifest().getVersion().toString());
+        LOGGER.atInfo().log("Initializing " + this.getName() + " version " + this.getManifest().getVersion().toString());
     }
 
     @Override
     protected void setup() {
-        LOGGER.atInfo().log("Setting up plugin " + this.getName());
-        this.getCommandRegistry().registerCommand(new ExampleCommand(this.getName(), this.getManifest().getVersion().toString()));
+        LOGGER.atInfo().log("Setting up " + this.getName());
     }
 }
